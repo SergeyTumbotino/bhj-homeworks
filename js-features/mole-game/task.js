a6c677615c4f9c60c46879d972a4e420a42a4074
Sergey Novikov <novsp78@gmail.com>
2022-12-08 22:07:55 +0300

    js-features2

--- js-features/mole-game/task.js
+++ js-features/mole-game/task.js
@@ -14,21 +14,22 @@ function getMole(index) {
   }
 }
 
-for (let i = 1; i < 10; i++) {
+for (let i = 0; i < 9; i++) {
   hole[i].addEventListener("click", () => {
     getMole(i);
-  });
-  if (deadCounter === 10 || lostCounter === 5) {
-    deadCounter = 0;
-    lostCounter = 0;
-    dead.textContent = deadCounter;
-    lost.textContent = lostCounter;
+
     if (deadCounter === 10) {
+      deadCounter = 0;
+      lostCounter = 0;
+      dead.textContent = deadCounter;
+      lost.textContent = lostCounter;
       alert("Победа!");
-      return;
     } else if (lostCounter === 5) {
+      deadCounter = 0;
+      lostCounter = 0;
+      dead.textContent = deadCounter;
+      lost.textContent = lostCounter;
       alert("Вы проиграли!");
-      return;
     }
-  }
+  });
 }
